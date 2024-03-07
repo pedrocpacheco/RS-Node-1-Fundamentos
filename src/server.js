708f492f2935a83a5e15921d669a7998bd3788f4
pedrocpacheco <cpedropacheco@hotmail.com>
2024-03-07 15:56:02 -0300

    fix: add / to urls

--- src/server.js
+++ src/server.js
@@ -6,19 +6,19 @@ const server = http.createServer((req, res) => {
   // const url = req.url
   const { method, url } = req
 
-  if (method === 'GET' && url === 'users')
+  if (method === 'GET' && url === '/users')
     res.end('Listagem de Usuarios')
 
-  if (method === 'POST' && url === 'users')
+  if (method === 'POST' && url === '/users')
     res.end('Criação de usuario')
 
-  if (method === 'PUT' && url === 'users')
+  if (method === 'PUT' && url === '/users')
     res.end('Atualização geral de Usuario')
 
-  if (method === 'PATCH' && url === 'users')
+  if (method === 'PATCH' && url === '/users')
     res.end('Atualização especifica de Usuario')
 
-  if (method === 'DELETE' && url === 'users')
+  if (method === 'DELETE' && url === '/users')
     res.end('Remoção de Usuario')
 
 })
