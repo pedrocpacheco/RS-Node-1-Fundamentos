fa851ad641c2bfb90adc53efedd82962067610cd
pedrocpacheco <cpedropacheco@hotmail.com>
2024-03-07 15:54:04 -0300

    feat: add http routes example

--- src/server.js
+++ src/server.js
@@ -2,7 +2,25 @@
 import http from 'node:http'
 
 const server = http.createServer((req, res) => {
-  return res.end('Hello World!')
+  // const method = req.method
+  // const url = req.url
+  const { method, url } = req
+
+  if (method === 'GET' && url === 'users')
+    res.end('Listagem de Usuarios')
+
+  if (method === 'POST' && url === 'users')
+    res.end('Criação de usuario')
+
+  if (method === 'PUT' && url === 'users')
+    res.end('Atualização geral de Usuario')
+
+  if (method === 'PATCH' && url === 'users')
+    res.end('Atualização especifica de Usuario')
+
+  if (method === 'DELETE' && url === 'users')
+    res.end('Remoção de Usuario')
+
 })
 
 server.listen(3333)
